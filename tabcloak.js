b16789c95b42355b7d2424e941caaab72c25e9a8
Math help <161599965+Glitchedcat1@users.noreply.github.com>
2025-05-26 19:06:12 -0400

    Create tabcloak.js

---
+++ tabcloak.js
@@ -0,0 +1,20 @@
+document.getElementById("unlock-btn").addEventListener("click", function () {
+  const password = prompt("Enter password to unlock features:");
+  if (password === "lexia123") {
+    document.getElementById("cloak-controls").style.display = "block";
+  } else {
+    alert("Incorrect password!");
+  }
+});
+
+document.getElementById("tabcloak-btn").addEventListener("click", function () {
+  document.title = "Lexia Core5";
+
+  let favicon = document.querySelector("link[rel~='icon']");
+  if (!favicon) {
+    favicon = document.createElement("link");
+    favicon.rel = "icon";
+    document.head.appendChild(favicon);
+  }
+  favicon.href = "lexia-favicon.png";
+});
